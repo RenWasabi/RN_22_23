5d81b4addf33fb4a8f573b4a4ccae7cf0c7fd701
Carolina Reiß <renwasabi@t-online.de>
2022-11-17 15:16:10 +0100

    added functionality for printing IP-Version and Address of addrinfo for testing (code from Beej)

--- praxis1/webserver.c
+++ praxis1/webserver.c
@@ -4,6 +4,8 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string.h> // for memset
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 
 int main(int argc, char** argv) {
@@ -11,6 +13,8 @@ int main(int argc, char** argv) {
     /* ACHTUNG: Beej's Guide to Network Programming wurde als Referenz benutzt,
      * während Kopien vermieden wurden, können dennoch einige Stellen
      * Ähnlichkeit haben. */
+
+    printf("Input Port: %s\n", argv[1]);
 
     // initialize the addrinfo structure for the listening socket
     struct addrinfo hints;
@@ -33,12 +37,43 @@ int main(int argc, char** argv) {
     // MISSING iterating over linked list starting add listener_addrinfo in case first result doesn't work
     int listener_fd = socket(listener_addrinfo->ai_family, listener_addrinfo->ai_socktype, listener_addrinfo->ai_protocol);
     if (listener_fd < 0){
-        printf("Error caling socket()\n");
+        printf("Error calling socket()\n");
         exit(1);
     }
     printf("Created Listener Socket. Socket-ID: %d\n", listener_fd);
 
+
+    // END OF IP-ADDRESS TESTING BLOCK
+    /* This code prints the IP-Address associated with the listener socket.
+     * It's for testing purposes.
+     * Except for some deviation in the naming, the code is directly taken from
+     * Beej's Guide to Network Programming, section 3.4 IP Addresses, Part Deux */
+    void* addr;
+    char* ipver;
+    struct sockaddr *listener_sockaddr = listener_addrinfo->ai_addr;
+    if (listener_addrinfo->ai_family == AF_INET){
+        struct sockaddr_in* listen_ipv4 =(struct sockaddr_in*) listener_sockaddr;
+        addr = &(listen_ipv4->sin_addr);
+        ipver = "IPv4";
+    }
+    else {
+        struct sockaddr_in6* listen_ipv6 = (struct sockaddr_in6*) listener_sockaddr;
+        addr = &(listen_ipv6->sin6_addr);
+        ipver = "IPv6";
+    }
+
+    int ip_buf_len = 50;
+    char* ip_address = calloc(ip_buf_len,sizeof(char));
+    inet_ntop(listener_addrinfo->ai_family, addr, ip_address, ip_buf_len);
+    printf("%s: %s\n", ipver, ip_address);
+    // END OF IP-ADDRESS TESTING BLOCK
+
+
     // bind listener socket to port
+    if (bind(listener_fd, listener_addrinfo->ai_addr, listener_addrinfo->ai_addrlen) < 0){
+        printf("Error calling bind()\n");
+        exit(1);
+    }
 
 
 
