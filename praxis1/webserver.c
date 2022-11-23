d7bc6243c67c34f2b64bf0610341e14472086b63
chiblick <103932288+chiblick@users.noreply.github.com>
2022-11-23 18:15:54 +0100

    Update webserver.c

--- praxis1/webserver.c
+++ praxis1/webserver.c
@@ -1,29 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h> // for testing
-#include <errno.h> // for recognizing errors
 #include <sys/types.h>
 #include <sys/socket.h>
-// #include <netinet/in.h
-#include "listen_respond.h"
-#include "helper_functions.h"
+#include <netdb.h>
+#include <string.h> // for memset
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <errno.h>
 
-/* cmake -B cmake-build-debug -DCMAKE_BUILD_TYPE=Debug
- * make -C cmake-build-debug
- * ./cmake-build-debug/webserver
- * */
+#define BACKLOG 20 // how many incoming requests can be queued before accept()
+//------------------------------DELETE----------------------------------------------------------------------
+//just for testing rn -> i need to somehow fill the struct addrinfo wwith the function getaddrinfo()
+//this try has no error handling whatsoever
 
 int main(int argc, char** argv) {
     // Start here :)
+    struct addrinfo hints, *result;     //result is a pointer to the resulting addrinfo
+    int rv;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM; // TCP streaming socket
+    hints.ai_flags = AI_PASSIVE; 
+
+    //using getaddrinfo() so we have the stuff ready for upcoming functions
+    //MISSING: Errorhandling
+    rv = getaddrinfo(NULL, argv[1], &hints, &result);
 
 
-    // get port parameter
-    char* port_string = argv[1];
-    unsigned short port = port_string_to_ul(port_string);
-    printf("Port number: %d\n", port);
+    //socket()
+    int sockfd;
+    sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 
+    //bind()
+    bind(sockfd, result->ai_addr, result->ai_addrlen);
 
+    //listen()
+    listen(sockfd, BACKLOG);
+    freeaddrinfo(result); // free list of getaddrinfo results
 
-    //listen_socket(port);
 
 
     return EXIT_SUCCESS;
