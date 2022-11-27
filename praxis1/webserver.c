feec03d4d8f0a9a1791f47f7fc5aa8ca4ed064be
Carolina Reiß <renwasabi@t-online.de>
2022-11-27 11:04:48 +0100

    added functionality and struct for administrating buffers

--- praxis1/webserver.c
+++ praxis1/webserver.c
@@ -11,6 +11,59 @@
 #define BACKLOG 20 // how many incoming requests can be queued before accept()
 #define BUFFER_SIZE 100 // size of the char buffer that recv() writes client requests to
 
+/* cmake -B cmake-build-debug -DCMAKE_BUILD_TYPE=Debug
+ * make -C cmake-build-debug
+ * ./cmake-build-debug/webserver
+ * */
+
+typedef struct buf_struct {
+    char* buffer; // the actual buffer
+    int available_space; // size of buffer in bytes
+    int used_space; // number of valid bytes (from beginning of buffer)
+    int scanner_index; // last position at which the buffer has been scanned for terminator sequence
+    int req_begin_index; // beginning of current request in buffer, always 0 for the secondary buffer
+} buf_struct;
+
+// create and initialize an administration structure for a buffer, including the buffer itself
+buf_struct* buf_struct_init(int buf_size){
+    if (buf_size <= 0){
+        fprintf(stderr, "Buffer can not be created with size %d\n", buf_size);
+        exit(1);
+    }
+    buf_struct* buf_struct = malloc(sizeof(buf_struct));
+    buf_struct->buffer = malloc(buf_size* sizeof(char)); // missing error handling
+    memset(buf_struct->buffer, 0, buf_size);
+    buf_struct->available_space = buf_size;
+    buf_struct->used_space = 0;
+    buf_struct->scanner_index = 0;
+    buf_struct->req_begin_index = 0;
+    printf("Created buf_struct with buffer size %d at address %p.\n", buf_struct->available_space, buf_struct);
+    return buf_struct;
+}
+
+void buf_struct_free(buf_struct* buf_to_free){
+    free(buf_to_free->buffer);
+    free(buf_to_free);
+    return;
+}
+
+
+buf_struct* buf_struct_expand(buf_struct* old_buf){
+    buf_struct* new_buf = buf_struct_init(old_buf->available_space*2);
+    strncpy(new_buf->buffer, old_buf->buffer, old_buf->used_space);
+    new_buf->used_space = old_buf->used_space;
+    new_buf->scanner_index = old_buf->scanner_index;
+    // no adjusting of req_begin_index since that's only relevant in the recv buffer, which will not
+    // be expanded
+    printf("Expanded old buffer at %p with content %s and size %d "
+           "to new buffer at %p with content %s and size %d\n.", \
+           old_buf, old_buf->buffer, old_buf->available_space, \
+        new_buf, new_buf->buffer, new_buf->available_space);
+    free(old_buf);
+    return new_buf;
+}
+
+
 /* compare whether the end of the valid buffer matches a specified terminator sequene
  * valid buffer is specified by how many bytes were received (=> written to buffer)
  * with recv() */
@@ -25,11 +78,20 @@ int cmp_buffend_terminator(char* buffer, int valid_bytes, char* terminator, int 
     return strings_same;
 }
 
+
+
 int main(int argc, char** argv) {
     // Start here :)
     /* ACHTUNG: Beej's Guide to Network Programming wurde als Referenz benutzt,
      * während Kopien vermieden wurden, können dennoch einige Stellen
      * Ähnlichkeit haben. */
+
+    // TEST
+    buf_struct* req_buf = buf_struct_init(40);
+    strncpy(req_buf->buffer, "Ein Test-String\n", sizeof("Ein Test-String\n"));
+    req_buf->used_space = sizeof ("Ein Test-String\n");
+    printf(req_buf->buffer);
+    req_buf = buf_struct_expand(req_buf);
 
     // initialize the addrinfo structure for the listening socket
     struct addrinfo hints;
@@ -125,16 +187,20 @@ int main(int argc, char** argv) {
     int terminator_length = 4;
     printf("Terminator length: %d\n", terminator_length);
 
+
         while(1) {
             errno = 0;
             int received_bytes = recv(connection_fd, buffer, sizeof(buffer), 0);
+            // recv() failed
             if (received_bytes < 0) {
                 perror("Error while receiving: ");
                 exit(1);
+            // client closed connection
             } else if (received_bytes == 0){
                 printf("The client closed the connection.\n");
                 //close(connection_fd);
                 exit(1);
+            // data was received successfully
             } else {
                 printf("Received %d bytes from client.\n", received_bytes);
                 printf("Received: %s\n", buffer);
@@ -149,6 +215,7 @@ int main(int argc, char** argv) {
                     if (cmp_buffend_terminator(buffer, received_bytes, http_terminator, terminator_length) == 0){
                         printf("Not a valid HTTP request.\n");
                     } else {
+                        printf("Valid HTML packet.\n");
                         if(send(connection_fd, reply_msg, sizeof(reply_msg)-1, 0) < 0) {
                             exit(1);}
                     }
@@ -173,3 +240,12 @@ int main(int argc, char** argv) {
 
     return EXIT_SUCCESS;
 }
+
+/* example of a request (can be recv with one recv()
+ * that contains more than one HTTP packet
+echo -en 'GET / HTTP/1.1\r\nHost:
+example.com\r\n\r\n GET / HTTP/1.1\r\nHost:
+example.com\r\n\r\n' | nc localhost 80
+ */
+
+
